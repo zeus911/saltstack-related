42793b0292b743bae8970cd94ae6365899db7aad
wcannon <william.cannon@gmail.com>
2013-09-27 14:20:33 -0500

    Update wc_ec2_metadata.py
    added default call of ipv4 in example, updated placement group url

--- _modules/wc_ec2_metadata.py
+++ _modules/wc_ec2_metadata.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import urllib2
 import logging
-
-TODO:  Add error handling
-TODO:  Add logging
+ 
 # Reference: http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AESDG-chapter-instancedata.html
 # MetaData we want to make available using salt: public-hostname, public-ipv4, instance-id, placement, security-groups
-
+ 
 log = logging.getLogger(__name__)
 mainurl = "http://169.254.169.254/latest/meta-data/"
-
+ 
 def get_public_dns():
   '''A function to retrieve the public dns address of a server
-
+ 
   CLI Example::
-    salt stage3 hp_dns.get_public_dns
+    salt myserver hp_dns.get_public_dns
   '''
   url = mainurl + "public-hostname"
   try:
@@ -24,12 +22,12 @@ def get_public_dns():
     log.exception(e)
     return "Error: %s" % e
   return output
-
+ 
 def get_public_ipv4():
   '''A function to retrieve the public ip v4 address of a server
-
+ 
   CLI Example::
-    salt stage3 hp_dns.get_public_ipv4
+    salt myserver hp_dns.get_public_ipv4
   '''
   url = mainurl + "public-ipv4"
   try:
@@ -39,12 +37,12 @@ def get_public_ipv4():
     log.exception(e)
     return "Error: %s" % e
   return output
-
+ 
 def get_instance_id():
   '''A function to retrieve the aws instance id of a server
-
+ 
   CLI Example::
-    salt stage3 hp_dns.get_instance_id
+    salt myserver hp_dns.get_instance_id
   '''
   url = mainurl + "instance-id"
   try:
@@ -54,14 +52,14 @@ def get_instance_id():
     log.exception(e)
     return "Error: %s" % e
   return output
-
+ 
 def get_placement():
   '''A function to retrieve the aws placement of a server
-
+ 
   CLI Example::
-    salt stage3 hp_dns.get_placement
+    salt myserver hp_dns.get_placement
   '''
-  url = mainurl + "placement"
+  url = mainurl + "placement/availability-zone"
   try:
     f = urllib2.urlopen(url)
     output = f.read()
@@ -69,12 +67,12 @@ def get_placement():
     log.exception(e)
     return "Error: %s" % e
   return output
-
+ 
 def get_security_groups():
   '''A function to retrieve the security groups an aws instance has been placed
-
+ 
   CLI Example::
-    salt stage3 hp_dns.get_security_groups
+    salt myserver hp_dns.get_security_groups
   '''
   url = mainurl + "security-groups"
   try:
@@ -84,10 +82,10 @@ def get_security_groups():
     log.exception(e)
     return "Error: %s" % e
   return output
-
+ 
 if __name__ == "__main__":
   print "public dns = %s" % get_public_dns()
+  print "ipv4 address = %s" % get_public_ipv4()
   print "instance id = %s" % get_instance_id()
   print "ec2 placement = %s" % get_placement()
   print "security groups = %s" % get_security_groups()
-
